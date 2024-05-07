80787c5c6c3555b62bdfde5ffdcff14f59ac579a
Jinkogule <lucaspimenta21@gmail.com>
2024-05-07 10:00:06 -0300

    separando processamento do post do index para o analise_basica

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('analise_basica/', views.analise_basica, name='analise_basica'),
+    path('analise_especifica/', views.analise_especifica, name='analise_especifica'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -6,25 +6,31 @@ from analise_apriori.forms import UploadFileForm
 
 def index(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            csv_file = request.FILES['file'].read().decode('utf-8')
-            data = StringIO(csv_file)
-            colunas = obter_colunas(csv_file)
-            try:
-                df = carregar_e_preprocessar_dados(data)
-            except ValueError as e:
-                return HttpResponse(str(e))
-            suporte_minimo = 0.01
-            try:
-                conjuntos_itens_frequentes = gerar_conjuntos_itens_frequentes(df, suporte_minimo)
-                regras = gerar_regras(conjuntos_itens_frequentes)
-                regras_html = regras.to_html()
-                
-            except ValueError as e:
-                return HttpResponse(f'Não é possível analisar esta base de dados sobre as configurações atuais (itemsets vazios para suporte mínimo={suporte_minimo})')
-            return render(request, 'main/index.html', {'regras': regras_html, 'colunas': colunas})
+        return analise_basica(request)
     else:
         form = UploadFileForm()
-    return render(request, 'main/index.html', {'form': form})
+        return render(request, 'main/index.html', {'form': form})
+    
+def analise_basica(request):
+    form = UploadFileForm(request.POST, request.FILES)
+    if form.is_valid():
+        csv_file = request.FILES['file'].read().decode('utf-8')
+        data = StringIO(csv_file)
+        colunas = obter_colunas(csv_file)
+        try:
+            df = carregar_e_preprocessar_dados(data)
+        except ValueError as e:
+            return HttpResponse(str(e))
+        suporte_minimo = 0.01
+        try:
+            conjuntos_itens_frequentes = gerar_conjuntos_itens_frequentes(df, suporte_minimo)
+            regras = gerar_regras(conjuntos_itens_frequentes)
+            regras_html = regras.to_html()
+        except ValueError as e:
+            return HttpResponse(f'Não é possível analisar esta base de dados sobre as configurações atuais (itemsets vazios para suporte mínimo={suporte_minimo})')
+        return render(request, 'main/analise_basica.html', {'regras': regras_html, 'colunas': colunas})
+
+def analise_especifica(request):
+    return HttpResponse(f'teste')
+
 
