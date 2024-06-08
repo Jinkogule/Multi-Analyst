fb276a69f759c282ca0a7d896458739ef1134f02
Jinkogule <lucaspimenta21@gmail.com>
2024-06-08 14:58:50 -0300

    att method detectar_encoding e add log messages

--- analise_apriori/utils.py
+++ analise_apriori/utils.py
@@ -1,14 +1,32 @@
 import csv
+import logging
 import chardet
 
-def detectar_encoding(csv_file):
-    try:
-        return csv_file.read().decode('utf-8')
-    except UnicodeDecodeError:
-        rawdata = csv_file.read()
-        resultado = chardet.detect(rawdata)
-        encoding = resultado['encoding'] if resultado['encoding'] else 'iso-8859-1'
-        return rawdata.decode(encoding)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler()])
+
+def detectar_encoding(arquivo_csv):
+    encodings = ['utf-8', 'iso-8859-1', 'windows-1252']
+    raw_data = arquivo_csv.read()
+    
+    for encoding in encodings:
+        try:
+            arquivo_csv.seek(0)
+            encoding_detectado = raw_data.decode(encoding)
+            logging.info(f"Decodificado com sucesso usando o encoding: {encoding}")
+            return encoding_detectado
+        except UnicodeDecodeError:
+            logging.warning(f"Falha ao decodificar usando o encoding: {encoding}")
+            continue
+
+    encoding_detectado = chardet.detect(raw_data)
+    encoding = encoding_detectado['encoding']
+    
+    if encoding:
+        logging.info(f"Encoding detectado usando chardet: {encoding}")
+        return encoding_detectado.decode(encoding)
+    else:
+        logging.error("Não foi possível detectar o encoding do arquivo CSV.")
+        raise UnicodeDecodeError("Não foi possível detectar o encoding do arquivo CSV.")
 
 def remover_todos_zeros_da_lista(lista):
     return [x for x in lista if x != 0]
